14dac9b24f99608f52dd162d651360d446a299c7
Qingyao Sun <sunqingyao19970825@icloud.com>
2023-01-17 23:56:08 -0600

    increase velocity model capacity

--- rectflow/velocity.py
+++ rectflow/velocity.py
@@ -9,17 +9,17 @@ class Velocity(eqx.Module):
     layers: Sequence
 
     def __init__(self, key):
-        key1, key2, key3 = jax.random.split(key, 3)
+        key1, key2, key3, key4, key5 = jax.random.split(key, 5)
         self.layers = [
-            eqx.nn.Linear(3, 512, key=key1),
-            jax.nn.relu,
-            eqx.nn.Linear(512, 1024, key=key2),
+            eqx.nn.Linear(3, 1024, key=key1),
             jax.nn.relu,
             eqx.nn.Linear(1024, 1024, key=key2),
             jax.nn.relu,
-            eqx.nn.Linear(1024, 512, key=key2),
+            eqx.nn.Linear(1024, 1024, key=key3),
             jax.nn.relu,
-            eqx.nn.Linear(512, 2, key=key3),
+            eqx.nn.Linear(1024, 1024, key=key4),
+            jax.nn.relu,
+            eqx.nn.Linear(1024, 2, key=key5),
         ]
 
     def __call__(self, z, t):
